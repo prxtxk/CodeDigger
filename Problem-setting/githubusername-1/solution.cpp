31327ec302476020d3e95a6fc2eb9b59482717f6
kenma-w <152846901+kenma-w@users.noreply.github.com>
2024-12-18 17:02:21 +0530

    Create solution.cpp

---
+++ Problem-setting/githubusername-1/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+
+void solve(){
+  int x;
+  cin>>n>>x;
+  int ans=0;
+  ans+=min(x-1,n-x);
+  ans+=(ceil(n*1.0/2)-ans-1)*2;
+  cout<<ans<<endl;
+ }
+    
+signed main(){ 
+  ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    
+     int t;
+     cin>>t;
+    while(t--){
+   solve();
+     }
+return 0;
+}
